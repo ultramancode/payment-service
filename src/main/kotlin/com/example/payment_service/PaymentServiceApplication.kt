7f9fec821d50faa067ac89eb10ab30cd23d1fc80
kimtaewoong <ktw2172@gmail.com>
2025-06-05 19:17:04 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/payment_service/PaymentServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.payment_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaymentServiceApplication>(*args)
+}
